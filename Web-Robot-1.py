f925e23de10ed46ec1f1071d9a1dcbe620409608
brunacrespomello <brunacrespomello@gmail.com>
2022-08-11 03:37:26 +0100

    Project 1: Domain Search Summary

--- Web-Robot-1.py
+++ Web-Robot-1.py
@@ -1,20 +1,36 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+# welcome
+
+print('Robot initiated')
+
+# initiate web drive
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-logging")
+options.add_argument("--log-level-3")
+
 # internet access
-driver = webdriver.Chrome('chromedriver/chromedriver')
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s, options=options)
 driver.get('https://www.registro.br')
 
+
+# search local to input
 search = driver.find_element(By.ID, 'is-avail-field')
 search.clear()
-search.send_keys('brunacrespomello')
+
+domain = 'brunacrespomello'
+search.send_keys(domain)
 search.send_keys(Keys.RETURN)
+time.sleep(2)
 
-
-time.sleep(8000)
+# getting bolder
+driver.find_element(By.XPATH, '//*[@id="app"]/main/section/div[2]/div/p/span/strong')
+print('Domain: %s %s' % (domain, driver.find_element(By.XPATH, '//*[@id="app"]/main/section/div[2]/div/p/span/strong')))
 driver.close()
-
-
-
