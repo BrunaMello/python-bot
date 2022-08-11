f84f02b2d6b68232c7d8abe2fd59275dc9cae813
brunacrespomello <brunacrespomello@gmail.com>
2022-08-11 02:37:22 +0100

    Introductions and Annotations

---
+++ Introduction and anotations.py
@@ -0,0 +1,37 @@
+# Basic pratice
+
+
+# print
+print("Hello, world!")
+
+# math
+print(5 + 3)
+print(5 - 3)
+
+# variable
+a = 5
+b = 3
+soma = a + b
+square = soma ** 2
+print('The square of:', soma, 'is:', square)
+
+# conditionals
+if soma == 10:
+    print('The sum is true')
+else:
+    print('The sum is false')
+
+# loops
+for n in range(5):  # range = repetitions
+    print('I like you')
+
+
+# + concatenate
+# % formating string
+
+#raw_input() - returns string
+
+
+
+
+
